ced7cf3e917f7e24b55a562baf45d9aee7270c1f
杨福照 <yangfuzhao@ipaynow.cn>
2017-10-09 17:59:53 +0800

    1

--- common/src/main/java/com/yangfuzhao/study/queue/DelayedElement.java
+++ common/src/main/java/com/yangfuzhao/study/queue/DelayedElement.java
@@ -1,7 +1,21 @@
 package com.yangfuzhao.study.queue;
 
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by ipaynow0929 on 2017/10/9.
  */
-public class DelayedElement {
+public class DelayedElement implements Delayed {
+    @Override
+    //获取失效时间
+    public long getDelay(TimeUnit unit) {
+        return 0;
+    }
+
+    @Override
+    //比较 1是放入队尾  -1是放入队头
+    public int compareTo(Delayed o) {
+        return 0;
+    }
 }
