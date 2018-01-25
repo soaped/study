90e1befc9d7836112010dee929a839dc6f6bb531
Unknown <510904625@qq.com>
2018-01-25 09:41:55 +0800

    1

--- spring_boot/src/main/java/com/study/xyls/controller/WorkColloter.java
+++ spring_boot/src/main/java/com/study/xyls/controller/WorkColloter.java
@@ -5,6 +5,7 @@ import com.study.xyls.req.GetWorkDataReq;
 import com.study.xyls.vo.MvcResponse;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class WorkColloter {
 
     @ApiOperation(value = "获取考勤数据",notes = "部门信息传部门名称")
     @RequestMapping("getWorkData")
-    public MvcResponse getWorkData(@Valid GetWorkDataReq getWorkDataReq, BindingResult bindingResult) {
+    public MvcResponse getWorkData(@Valid @RequestBody GetWorkDataReq getWorkDataReq, BindingResult bindingResult) {
        // CommonResp commonResp = workDataService.getWorkDataList(getWorkDataReq);
         return MvcResponse.generrateSucessMvcResponse("");
     }
--- spring_boot/src/main/java/com/study/xyls/req/GetWorkDataReq.java
+++ spring_boot/src/main/java/com/study/xyls/req/GetWorkDataReq.java
@@ -1,15 +1,18 @@
 package com.study.xyls.req;
 
-import com.ipaynow.ipaynow_user.mvc_support.params.req.BaseReqPaginator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * Created by ipaynow0929 on 2018/1/11.
  */
 @Data
+@ApiModel("部门参数")
 public class GetWorkDataReq extends BaseReqPaginator {
 
     //@NotNull(message = "部门不能为空;")
+    @ApiModelProperty(value = "部门ID",required = true,example = "7")
     private String dept;
 
     //@NotNull(message = "工号不能为空;")
