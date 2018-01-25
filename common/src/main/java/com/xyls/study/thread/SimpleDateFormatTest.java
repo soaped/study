fbec7587801070ece92a56f867b3f5449185d5ba
Unknown <510904625@qq.com>
2018-01-25 10:23:19 +0800

    1

---
+++ common/src/main/java/com/xyls/study/thread/SimpleDateFormatTest.java
@@ -0,0 +1,28 @@
+package com.xyls.study.thread;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author ipaynow on 2018/1/25
+ */
+public class SimpleDateFormatTest {
+    //(1)创建单例实例
+    static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static void main(String[] args) {
+        //(2)创建多个线程，并启动
+        for (int i = 0; i < 11; ++i) {
+            Thread thread = new Thread(new Runnable() {
+                public void run() {
+                    try {//(3)使用单例日期实例解析文本
+                        System.out.println(sdf.parse("2017-12-13 15:17:27"));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            thread.start();//(4)启动线程
+        }
+    }
+}
